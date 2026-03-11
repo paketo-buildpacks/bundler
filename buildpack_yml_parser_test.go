498f9096740b63c12897a9b5d51e1ae739214994
BG <bguttmann@anynines.com>
2026-03-11 09:06:13 +0100

    Fix linter issues + unit tests

--- buildpack_yml_parser_test.go
+++ buildpack_yml_parser_test.go
@@ -21,7 +21,9 @@ func testBuildpackYMLParser(t *testing.T, context spec.G, it spec.S) {
 	it.Before(func() {
 		file, err := os.CreateTemp("", "buildpack.yml")
 		Expect(err).NotTo(HaveOccurred())
-		defer file.Close()
+		defer func() {
+			Expect(file.Close()).To(Succeed())
+		}()
 
 		_, err = file.WriteString(`---
 bundler:
